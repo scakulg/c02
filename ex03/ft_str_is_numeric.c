b5f734d2770bca280765f778ff29e1d3c43211a7
Gergő Lukács <gergolukacs@Gergos-MacBook-Air.local>
2023-08-24 14:16:59 +0200

    c02

---
+++ ex03/ft_str_is_numeric.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int ft_str_is_numeric(char *str)
+{
+	int	count;
+
+	count = 0;
+	if (str != 0)
+	{
+		while(str[count] != '\0')
+		{
+			count++;
+			if(str[count] >= 48 && str[count] <= 57)
+				return(1);
+			else
+				return(0);
+		}
+	}
+	return (1);
+}
+
+int	main()
+{
+
+	printf ("%d",ft_str_is_numeric(""));
+
+	return (0);
+}
