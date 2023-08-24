314be07e4897aaaae06e8a5e032cddab895563c2
Gergő Lukács <gergolukacs@Gergos-MacBook-Air.local>
2023-08-24 19:09:55 +0200

    c02

---
+++ ex09/ft_strcapitalize.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+char	*ft_strcapitalize(char *str)
+{
+	int i;
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	i = 1;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (!(str[i - 1] >= 'a' && str[i - 1] <= 'z'))
+			{
+				if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
+				{
+					if (!(str[i - 1] <= 'Z' && str[i - 1] >= 'A'))
+					{
+						str[i] -= 32;
+					}
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+
+int	main()
+{
+	char	c[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	printf("%s", ft_strcapitalize(c));
+	return (0);
+}
